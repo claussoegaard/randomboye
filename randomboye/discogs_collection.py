45c2add5d2b3fe556c1c3322b2c1345543eac565
Claus <claussoegaard@gmail.com>
2020-01-08 17:27:38 -0800

    Adding basic logging and fine tuned discogs class

--- randomboye/discogs_collection.py
+++ randomboye/discogs_collection.py
@@ -3,23 +3,35 @@ import json
 import discogs_client
 import os
 
+import logging
+from logging.config import dictConfig
+from logging_config import LOGGING_CONFIG
+
+dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+FUNCTION_CALL_MSG = 'function_call'
+
 
 class DiscogsCollection():
-    def __init__(self, token, collection_file_name="discogs_collection.txt", update=False):
+    def __init__(self, token, collection_file_name="discogs_collection.txt", refresh_collection=False):
+        logger.debug(FUNCTION_CALL_MSG)
+        self._collection = {}
         self.token = token
-        self.collection_file_name = collection_file_name
-        self.update = update
-        if not os.path.isfile(self.absolute_collection_file_path):
-            self.update = True
+        # self.collection_file_name = collection_file_name
+        self.absolute_collection_file_path = f"{os.path.dirname(__file__)}/{collection_file_name}"
+        self.collection_file_exists = os.path.isfile(self.absolute_collection_file_path)
+        if not self.collection_file_exists:
+            self._collection = self.get_collection_from_discogs()
             self.write_collection_to_file()
-            self.update = update
-
-    @property
-    def absolute_collection_file_path(self):
-        return f"{os.path.dirname(__file__)}/{self.collection_file_name}"
+        elif self.collection_file_exists and refresh_collection:
+            self._collection = self.get_collection_from_discogs()
+            self.write_collection_to_file()
+        elif self.collection_file_exists and not refresh_collection:
+            self._collection = self.get_collection_from_file()
 
     @property
     def identity(self):
+        logger.debug(FUNCTION_CALL_MSG)
         client = discogs_client.Client(
             'RandomDiscogsRecord/0.1',
             user_token=f"{self.token}"
@@ -28,60 +40,47 @@ class DiscogsCollection():
 
     @property
     def collection(self):
-        if self.update:
-            discogs_collection = self.identity.collection_folders[0]
-            collection = {}
-            collection['record_count'] = discogs_collection.count
-            records = []
-            for release in discogs_collection.releases:
-                record = {}
-                record['artist'] = release.release.artists[0].name
-                record['title'] = release.release.title
-                records.append(record)
-            collection['records'] = records
-        else:
-            with open(self.absolute_collection_file_path, "r") as f:
-                collection = json.load(f)
+        return self._collection
+
+    def get_collection_from_discogs(self):
+        discogs_collection = self.identity.collection_folders[0]
+        collection = {}
+        collection['record_count'] = discogs_collection.count
+        records = []
+        for release in discogs_collection.releases:
+            record = {}
+            record['artist'] = release.release.artists[0].name
+            record['title'] = release.release.title
+            records.append(record)
+        collection['records'] = records
+        logger.info(f"Collection with {collection['record_count']} records fetched from discogs.")
+        return collection
+
+    def get_collection_from_file(self):
+        with open(self.absolute_collection_file_path, "r") as f:
+            collection = json.load(f)
+        logger.info(f"Collection with {collection['record_count']} records fetched from disk.")
         return collection
 
     def write_collection_to_file(self):
         try:
-            with open(self.absolute_collection_file_path, "w+") as f:
+            with open(self.absolute_collection_file_path, "w") as f:
                 try:
-                    data = json.load(f)
-                except ValueError:
-                    data = {}
-                # Only overwrite contents of file if record count has changed
-                if 'record_count' not in data or \
-                        data.get('record_count') != \
-                        self.collection.get('record_count'):
+                    # collection = json.load(f)
                     f.seek(0)
                     json.dump(self.collection, f)
                     f.truncate()
-        except Exception as e:
-            print("Something went wrong:")
-            print(e)
+                    logger.info(f"Collection with {self.collection['record_count']} records written to disk.")
+                except ValueError as e:
+                    logger.error(e)
 
-    # def get_random_record(self):
+        except Exception as e:
+            logger.error(e)
 
     def get_random_record(self):
-        record = {}
+        random_record = {}
         records_in_collection = self.collection['record_count']
         record_number = randint(0, records_in_collection - 1)
-        record['artist'] = self.collection['records'][record_number]['artist']
-        record['title'] = self.collection['records'][record_number]['title']
-        return record
-        # with open(self.absolute_collection_file_path, "r") as f:
-        #     data = json.load(f)
-        #     records_in_collection = data['record_count']
-        #     record_number = randint(0, records_in_collection - 1)
-        #     record['artist'] = data['records'][record_number]['artist']
-        #     record['title'] = data['records'][record_number]['title']
-        # return record
-
-
-my_collection = DiscogsCollection("CWhLmAdMsIhLXCeIfCXDZszstBbeWxbsTlmvntFf")
-
-# my_collection.write_collection_to_file()
-# print(my_collection.collection_file_exists)
-print(my_collection.get_random_record())
+        random_record['artist'] = self.collection['records'][record_number]['artist']
+        random_record['title'] = self.collection['records'][record_number]['title']
+        return random_record
---
+++ randomboye/logging_config.py
@@ -0,0 +1,90 @@
+import os
+LOGGING_FOLDER = "logs"
+
+# Creating logs dir if it doesn't already exist
+if not os.path.exists(LOGGING_FOLDER):
+    os.makedirs(LOGGING_FOLDER)
+
+# Good example of configs:
+# https://stackoverflow.com/questions/7507825/where-is-a-complete-example-of-logging-config-dictconfig
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        '': {  # root logger
+            'level': 'NOTSET',
+            'handlers': ['info_console_handler', 'debug_file_handler', 'info_file_handler', 'error_file_handler'],
+        },
+        'discogs_collection': {
+            'level': 'DEBUG',
+            'propagate': False,
+            'handlers': ['info_console_handler', 'debug_file_handler', 'info_file_handler', 'error_file_handler'],
+        },
+        # 'looper': {
+        #     'level': 'INFO',
+        #     'propagate': False,
+        #     'handlers': ['looper_info_file_handler', 'looper_error_file_handler']
+        # },
+    },
+    'handlers': {
+        'info_console_handler': {
+            'level': 'INFO',
+            'formatter': 'info',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+        'debug_file_handler': {
+            'level': 'DEBUG',
+            'formatter': 'debug',
+            'class': 'logging.FileHandler',
+            'filename': f'{LOGGING_FOLDER}/debug.log',
+            'mode': 'a',
+        },
+        'info_file_handler': {
+            'level': 'INFO',
+            'formatter': 'info',
+            'class': 'logging.FileHandler',
+            'filename': f'{LOGGING_FOLDER}/info.log',
+            'mode': 'a',
+        },
+        # 'looper_info_file_handler': {
+        #     'level': 'INFO',
+        #     'formatter': 'looper_info',
+        #     'class': 'logging.FileHandler',
+        #     'filename': 'looper_info.log',
+        #     'mode': 'a',
+        # },
+        'error_file_handler': {
+            'level': 'WARNING',
+            'formatter': 'error',
+            'class': 'logging.FileHandler',
+            'filename': f'{LOGGING_FOLDER}/error.log',
+            'mode': 'a',
+        },
+        # 'looper_error_file_handler': {
+        #     'level': 'WARNING',
+        #     'formatter': 'looper_error',
+        #     'class': 'logging.FileHandler',
+        #     'filename': 'looper_error.log',
+        #     'mode': 'a',
+        # },
+    },
+    'formatters': {
+        'debug': {
+            'format': '%(asctime)s :: %(levelname)s :: %(name)s :: %(module)s :: %(funcName)s :: %(lineno)s :: %(message)s'
+        },
+        'info': {
+            'format': '%(asctime)s :: %(levelname)s :: %(name)s :: %(module)s :: %(funcName)s :: %(lineno)s :: %(message)s'
+        },
+        # 'looper_info': {
+        #     'format': '%(asctime)s :: %(levelname)s :: %(name)s :: %(module)s :: %(threadName)s :: %(thread)d :: %(lineno)s :: %(message)s'
+        # },
+        'error': {
+            'format': '%(asctime)s :: %(levelname)s :: %(name)s :: %(process)d :: %(module)s :: %(funcName)s :: %(lineno)s :: %(message)s'
+        },
+        # 'looper_error': {
+        #     'format': '%(asctime)s :: %(levelname)s :: %(name)s :: %(process)d :: %(module)s :: %(threadName)s :: %(thread)d :: %(lineno)s :: %(message)s'
+        # },
+    },
+}
