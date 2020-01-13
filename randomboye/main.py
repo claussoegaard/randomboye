a8a4fbcf2ba2e841cde7e3c5a201e8c64c606045
Claus <claussoegaard@gmail.com>
2020-01-13 15:18:49 -0800

    Init commit on from-scratch branch

---
+++ randomboye/main.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    print("Ran Main Randomboye")
+
+    run = True
+    while run:
+        pass
+        # pause()
+        # pygame.time.delay(100)
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         run = False
+
+
+if __name__ == '__main__':
+    main()
