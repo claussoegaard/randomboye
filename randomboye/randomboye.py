f6418aa5c028096a6bcada04c428b48f85012b8f
Claus <claussoegaard@gmail.com>
2020-01-15 17:54:33 -0800

    Raspi signal

--- randomboye/randomboye.py
+++ randomboye/randomboye.py
@@ -1,5 +1,7 @@
 from definitions import FUNCTION_CALL_MSG
 from randomboye.discogs_collection import DiscogsCollection
+import os
+import signal
 # from multiprocessing import Process
 from logs.config import logger
 logger = logger(__name__)
@@ -7,11 +9,13 @@ logger = logger(__name__)
 
 def when_held_override():
     logger.debug(FUNCTION_CALL_MSG)
+    os.kill(pi, signal.SIGUSR1)
 
 
 def start(auth_token, is_test, refresh_collection):
     if not is_test:
         from randomboye.raspi import RaspberryPi
+        global pi
         pi = RaspberryPi()
         pi.start()
         logger.debug("After Raspberry Pi Is Init")
@@ -22,3 +26,7 @@ def start(auth_token, is_test, refresh_collection):
 
     dc = DiscogsCollection(auth_token=auth_token, refresh_collection=refresh_collection)
     dc.get_random_record()
+
+    pi.join()
+
+    logger.debug("After Raspberry Pi Is Shut Down")
