dfe218058b530db1fe2f314862527cd59254a4fb
Claus <claussoegaard@gmail.com>
2020-01-15 16:45:37 -0800

    More raspi stuff

--- randomboye/raspi.py
+++ randomboye/raspi.py
@@ -7,7 +7,6 @@ from RPi import GPIO
 from definitions import FUNCTION_CALL_MSG
 import os
 from signal import pause
-# import threading
 
 from logs.config import logger
 logger = logger(__name__)
@@ -18,6 +17,7 @@ class RaspberryPi(object):
         super().__init__()
         logger.debug(f"{FUNCTION_CALL_MSG}, {__class__}")
         GPIO.setwarnings(False)
+        self.run = True
         self.front_button_gpio = 4
         self.back_button_gpio = 3
         self.back_led_gpio = 14  # power LED
@@ -73,7 +73,8 @@ class RaspberryPi(object):
 
         self.write_framebuffer(self.default_startup_framebuffer())
 
-        pause()
+        if self.run:
+            pause()
 
     def shutdown(self, hold_time=6):
         # find how long the button has been held
@@ -139,76 +140,9 @@ class RaspberryPi(object):
 
     def back_button__when_held(self):
         logger.debug(FUNCTION_CALL_MSG)
-        self.shutdown()
+        self.run = False
+        # self.shutdown()
 
     def back_button__when_released(self):
         logger.debug(FUNCTION_CALL_MSG)
         self.back_led.off()
-
-
-# class PiButton(Button, threading.Thread):
-#     def __init__(self, pin, bounce_time, hold_time):
-#         logger.debug(f"{FUNCTION_CALL_MSG}, {__class__}")
-#         super().__init__(pin=pin, bounce_time=bounce_time, hold_time=hold_time)
-#         self.press_event = threading.Event()
-#         self.hold_event = threading.Event()
-#         self.release_event = threading.Event()
-#         self.when_pressed = self.set_press_event
-#         self.when_held = self.set_hold_event
-#         self.when_released = self.set_release_event
-
-#     def set_press_event(self):
-#         logger.debug(FUNCTION_CALL_MSG)
-#         self.press_event.set()
-
-#     def set_hold_event(self):
-#         logger.debug(FUNCTION_CALL_MSG)
-#         self.hold_event.set()
-
-#     def set_release_event(self):
-#         logger.debug(FUNCTION_CALL_MSG)
-#         self.release_event.set()
-
-#     def press_action(self):
-#         logger.debug(FUNCTION_CALL_MSG)
-#         self.press_event.wait()
-#         logger.debug("Pressed")
-
-#     def hold_action(self):
-#         logger.debug(FUNCTION_CALL_MSG)
-#         self.hold_event.wait()
-#         self.press_event.clear()
-#         logger.debug("Held")
-
-#     def release_action(self):
-#         logger.debug(FUNCTION_CALL_MSG)
-#         self.release_event.wait()
-#         self.hold_event.clear()
-#         self.release_event.clear()
-#         logger.debug("Released")
-
-#     def run(self):
-#         threading.Thread(name='press', target=self.press_action)
-#         threading.Thread(name='hold', target=self.hold_action)
-#         threading.Thread(name='release', target=self.release_action)
-
-
-"""
-Times Front Button:
-('04:55:34,118', '04:55:34,317'),
-('04:55:55,979', '04:55:56,241'),
-('04:58:49,928', '04:58:50,050'),
-('04:59:12,629', '04:59:12,887'),
-('04:59:40,564', '04:59:40,756'),
-('05:00:00,903', '05:00:01,264'),
-('05:00:19,886', '05:00:19,994')
-
-('04:57:40,652', '04:57:40,745') --No bug
-('04:58:10,908', '04:58:11,013') --No bug
-
-('', '')
-('', '')
-('', '')
-('', '')
-('', '')
-"""
