657beb4477f8d1f4fb85bfd4889cd78f9334e40c
Claus <claussoegaard@gmail.com>
2020-01-16 16:25:00 -0800

    Raspi signal

--- randomboye/randomboye.py
+++ randomboye/randomboye.py
@@ -14,6 +14,8 @@ class RandomBoye(object):
         self.dc = DiscogsCollection(auth_token=auth_token, refresh_collection=refresh_collection)
         self.pi = self.get_pi(is_test)
         self.pi.start()
+        self.pi.front_button.latest_event = None
+        self.pi.back_buton.latest_event = None
         self.pi.front_button.when_pressed = self.front_button_press_override
 
     def get_pi(self, is_test):
@@ -32,10 +34,28 @@ class RandomBoye(object):
 
     def front_button_press_override(self):
         logger.debug(FUNCTION_CALL_MSG)
-        random_record = self.dc.get_random_record()
-        artist_title = [random_record['record']['artist'], random_record['record']['title']]
-        record = create_framebuffers(artist_title)
-        self.pi.write_framebuffers(record)
+        self.pi.front_button.latest_event = 'press'
+
+    def front_button_hold_override(self):
+        logger.debug(FUNCTION_CALL_MSG)
+        self.pi.front_button.latest_event = 'hold'
+
+    def front_button_relase_override(self):
+        logger.debug(FUNCTION_CALL_MSG)
+        try:
+            if self.pi.front_button.latest_event:
+                if self.pi.front_button.latest_event == 'hold':
+                    logger.debug("Release After Hold - No Action")
+                if self.pi.front_button.latest_event == 'release':
+                    logger.debug("Release After Release - No Action")
+                if self.pi.front_button.latest_event == 'press':
+                    logger.debug("Release After Press - Random Record Flip")
+                    random_record = self.dc.get_random_record()
+                    artist_title = [random_record['record']['artist'], random_record['record']['title']]
+                    record = create_framebuffers(artist_title)
+                    self.pi.write_framebuffers(record)
+        finally:
+            self.pi.front_button.latest_event = 'release'
 
 
 # def start(auth_token, is_test, refresh_collection):
