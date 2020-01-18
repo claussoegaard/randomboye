c051123fc95019c301e88ec5073d8ed77d0296db
Claus <claussoegaard@gmail.com>
2020-01-18 11:53:59 -0800

    LCD Printer

--- remember_to_delete.py
+++ remember_to_delete.py
@@ -7,5 +7,5 @@ pi.start()
 time.sleep(2)
 
 for i in range(10):
-    print_job = (["Pavement       {i}", "Slanted And Ench"])
+    print_job = ([f"Pavement       {i}", "Slanted And Ench"])
     pi.add_to_print_jobs_queue(print_job)
