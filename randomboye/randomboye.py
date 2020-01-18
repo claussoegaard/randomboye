51f8a1c2a4a1a85a794e2ee818f5b35363fad940
Claus <claussoegaard@gmail.com>
2020-01-17 18:05:12 -0800

    Back button as sudo poweroff

--- randomboye/randomboye.py
+++ randomboye/randomboye.py
@@ -167,6 +167,8 @@ class RandomBoye(object):
                     latest_hold_time = self.pi.back_button.latest_hold_time
                     logger.debug(f"Release After Hold (Back, {latest_hold_time} seconds) - No Action")
                     if self.pi.back_button.was_latest_hold_long():
+                        self.cleanup()
+                        self.start_print_process(["Byeeeee..."])
                         self.pi.shutdown2()
 
                 if self.pi.back_button.latest_event == 'release':
