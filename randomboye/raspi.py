7b91cbceced5772426b3b67f6193beef74fdff32
Claus <claussoegaard@gmail.com>
2020-01-18 11:38:43 -0800

    LCD Printer

--- randomboye/raspi.py
+++ randomboye/raspi.py
@@ -7,7 +7,7 @@ from RPi import GPIO
 from definitions import FUNCTION_CALL_MSG
 import os
 import signal
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import time
 from randomboye.helpers import (
     # create_framebuffers,
@@ -70,193 +70,175 @@ class RaspberryPi(Process):
 
         self.back_led = LED(self.back_led_gpio)
 
-        self.startup_method = self.set_startup_method
+        # self.startup_method = self.set_startup_method
 
-    def lcd_cleanup(self):
-        logger.debug(FUNCTION_CALL_MSG)
-        self.lcd.home()
-        self.lcd.clear()
+        self.print_jobs = Queue()
+        self.lcd_printer = self.LCDFramebufferPrinter(self)
 
-    def shutdown2(self):
-        logger.debug(FUNCTION_CALL_MSG)
-        if self.shutdown_system:
-            logger.debug("Shutting down system")
-            self.back_led.on()
-            os.system("sudo poweroff")
-        else:
-            logger.debug("Shutting down Pi process")
-            # This throws some gpiozero related error on exit, but oh well
-            os.kill(self.pid, signal.SIGUSR1)
+    # def lcd_cleanup(self):
+    #     logger.debug(FUNCTION_CALL_MSG)
+    #     self.lcd.home()
+    #     self.lcd.clear()
 
-    def shutdown(self, hold_time=6):
-        logger.debug(FUNCTION_CALL_MSG)
-        # find how long the button has been held
-        p = self.back_button.pressed_time
-        logger.debug(f"Held for {p} seconds")
-        # blink rate will increase the longer we hold
-        # the button down. E.g., at 2 seconds, use 1/4 second rate.
-        self.back_led.blink(on_time=0.5 / p, off_time=0.5 / p)
-        if p > hold_time:
-            # Depending on system, either shutdown
-            # whole system, or just the object process
-            if self.shutdown_system:
-                logger.debug("Shutting down system")
-                self.back_led.on()
-                os.system("sudo poweroff")
-            else:
-                logger.debug("Shutting down Pi process")
-                # This throws some gpiozero related error on exit, but oh well
-                os.kill(self.pid, signal.SIGUSR1)
+    # def write_framebuffers(self, framebuffers,
+    #                        start_delay=3, end_delay=2, scroll_delay=0.4,
+    #                        end_on_start=True):
+    #     """Writes N framebuffers to LCD screen.
+    #     If len(framebuffers) > 1 it will first delay scrolling by start_delay,
+    #     then it will progress through framebuffers at scroll_delay speed,
+    #     until it reaches the end, where it will pause for end_delay time.
+    #     See write_framebuffer() for details on valid framebuffers.
+    #     Up to caller to implement a loop if desired, and to construct
+    #     valid framebuffers.
+    #     """
+    #     logger.debug(FUNCTION_CALL_MSG)
 
-    def write_framebuffer(self, framebuffer):
-        """Writes single framebuffer to LCD screen.
-        Framebuffer must fill entire LCD screen.
-        Valid framebuffer for 2x16 LCD:
-        ["I Am A Raspberry", "Pi                "]
-        No need to clear LCD since all cells will be overwritten
-        """
-        logger.debug(FUNCTION_CALL_MSG)
-        if len(framebuffer) != self.lcd_rows:
-            error = f"framebuffer must have exactly {self.lcd_rows} rows, has {len(framebuffer)}"
-            raise ValueError(error)
-        if not all([len(row) == self.lcd_cols and isinstance(row, str) for row in framebuffer]):
-            logger.debug(framebuffer)
-            error = f"all rows in framebuffer must be strings, exactly {self.lcd_cols} characters long"
-            raise ValueError(error)
-        self.lcd.home()
-        for row in framebuffer:
-            try:
-                self.lcd.write_string(row)
-                self.lcd.crlf()
-            except Exception as e:
-                print("Something went wrong:")
-                print(e)
-                return
+    #     # Overriding stuff if framebuffers only has one frame
+    #     if len(framebuffers) == 1:
+    #         end_on_start = False
+    #         start_delay = 0
+    #     self.pi.lcd.clear()  # Clearing once in beginning of framebuffer
+    #     for i, framebuffer in enumerate(framebuffers):
+    #         self.write_framebuffer(framebuffer)
+    #         if i == 0:
+    #             time.sleep(start_delay)
+    #         elif i == len(framebuffers) - 1:
+    #             time.sleep(end_delay)
+    #         else:
+    #             time.sleep(scroll_delay)
+    #     if end_on_start:
+    #         self.write_framebuffer(framebuffers[0])
 
-    def write_framebuffers(self, framebuffers,
-                           start_delay=3, end_delay=2, scroll_delay=0.4,
-                           end_on_start=True):
-        """Writes N framebuffers to LCD screen.
-        If len(framebuffers) > 1 it will first delay scrolling by start_delay,
-        then it will progress through framebuffers at scroll_delay speed,
-        until it reaches the end, where it will pause for end_delay time.
-        See write_framebuffer() for details on valid framebuffers.
-        Up to caller to implement a loop if desired, and to construct
-        valid framebuffers.
-        """
-        logger.debug(FUNCTION_CALL_MSG)
+    # def stream_multiples_of_lines(self, lines_list,
+    #                               start_delay=3, end_delay=2, scroll_delay=0.4,
+    #                               lines_delay=0.5, end_on_start=True):
+    #     """
+    #     lines = [line1, ..., lineN]
+    #     ergo:
+    #     lines_list = [[line1, ..., lineN], ..., [line1, ..., lineN]]
+    #     For each set of lines in lines_list, will do the following:
+    #     1. Create framebuffers for streaming
+    #     2. Write the first framebuffer then wait start_delay seconds
+    #     3. Write the remaining framebuffers with a scroll_delay gap
+    #     4. Wait end_delay at the last framebuffer
+    #     5. If end_on_start==True it will then write the first framebuffer again
+    #     Then after lines_delay it will do the same for the next line in
+    #     lines_list
+    #     """
+    #     logger.debug(FUNCTION_CALL_MSG)
+    #     if len(lines_list) == 1:
+    #         lines_delay = 0
+    #     multiple_framebuffers = create_multiple_framebuffers(lines_list)
+    #     for framebuffers in multiple_framebuffers:
+    #         self.write_framebuffers(
+    #             framebuffers=framebuffers,
+    #             start_delay=start_delay,
+    #             end_delay=end_delay,
+    #             scroll_delay=scroll_delay,
+    #             end_on_start=end_on_start
+    #         )
+    #         time.sleep(lines_delay)
 
-        # Overriding stuff if framebuffers only has one frame
-        if len(framebuffers) == 1:
-            end_on_start = False
-            start_delay = 0
-        self.lcd.clear()  # Clearing once in beginning of framebuffer
-        for i, framebuffer in enumerate(framebuffers):
-            self.write_framebuffer(framebuffer)
-            if i == 0:
-                time.sleep(start_delay)
-            elif i == len(framebuffers) - 1:
-                time.sleep(end_delay)
-            else:
-                time.sleep(scroll_delay)
-        if end_on_start:
-            self.write_framebuffer(framebuffers[0])
+    # def stream_lines(self, lines,
+    #                  start_delay=3, end_delay=2, scroll_delay=0.4,
+    #                  lines_delay=0.5, end_on_start=True):
+    #     """
+    #     Its not very intuitive to pass [[line1, ..., lineN]] to stream_multiples_of_lines
+    #     when you just want to write a single line, so this just wraps
+    #     [line1, ..., lineN] in a list and passes to stream_multiples_of_lines
+    #     """
+    #     lines_list = [lines]
+    #     self.stream_multiples_of_lines(
+    #         lines_list=lines_list,
+    #         start_delay=start_delay,
+    #         end_delay=end_delay,
+    #         scroll_delay=scroll_delay,
+    #         lines_delay=0,
+    #         end_on_start=end_on_start
+    #     )
 
-    def stream_multiples_of_lines(self, lines_list,
-                                  start_delay=3, end_delay=2, scroll_delay=0.4,
-                                  lines_delay=0.5, end_on_start=True):
-        """
-        lines = [line1, ..., lineN]
-        ergo:
-        lines_list = [[line1, ..., lineN], ..., [line1, ..., lineN]]
-        For each set of lines in lines_list, will do the following:
-        1. Create framebuffers for streaming
-        2. Write the first framebuffer then wait start_delay seconds
-        3. Write the remaining framebuffers with a scroll_delay gap
-        4. Wait end_delay at the last framebuffer
-        5. If end_on_start==True it will then write the first framebuffer again
-        Then after lines_delay it will do the same for the next line in
-        lines_list
-        """
-        logger.debug(FUNCTION_CALL_MSG)
-        if len(lines_list) == 1:
-            lines_delay = 0
-        multiple_framebuffers = create_multiple_framebuffers(lines_list)
-        for framebuffers in multiple_framebuffers:
-            self.write_framebuffers(
-                framebuffers=framebuffers,
-                start_delay=start_delay,
-                end_delay=end_delay,
-                scroll_delay=scroll_delay,
-                end_on_start=end_on_start
-            )
-            time.sleep(lines_delay)
+    # def shutdown2(self):
+    #     logger.debug(FUNCTION_CALL_MSG)
+    #     if self.shutdown_system:
+    #         logger.debug("Shutting down system")
+    #         self.back_led.on()
+    #         os.system("sudo poweroff")
+    #     else:
+    #         logger.debug("Shutting down Pi process")
+    #         # This throws some gpiozero related error on exit, but oh well
+    #         os.kill(self.pid, signal.SIGUSR1)
 
-    def stream_lines(self, lines,
-                     start_delay=3, end_delay=2, scroll_delay=0.4,
-                     lines_delay=0.5, end_on_start=True):
-        """
-        Its not very intuitive to pass [[line1, ..., lineN]] to stream_multiples_of_lines
-        when you just want to write a single line, so this just wraps
-        [line1, ..., lineN] in a list and passes to stream_multiples_of_lines
-        """
-        lines_list = [lines]
-        self.stream_multiples_of_lines(
-            lines_list=lines_list,
-            start_delay=start_delay,
-            end_delay=end_delay,
-            scroll_delay=scroll_delay,
-            lines_delay=0,
-            end_on_start=end_on_start
-        )
+    # def shutdown(self, hold_time=6):
+    #     logger.debug(FUNCTION_CALL_MSG)
+    #     # find how long the button has been held
+    #     p = self.back_button.pressed_time
+    #     logger.debug(f"Held for {p} seconds")
+    #     # blink rate will increase the longer we hold
+    #     # the button down. E.g., at 2 seconds, use 1/4 second rate.
+    #     self.back_led.blink(on_time=0.5 / p, off_time=0.5 / p)
+    #     if p > hold_time:
+    #         # Depending on system, either shutdown
+    #         # whole system, or just the object process
+    #         if self.shutdown_system:
+    #             logger.debug("Shutting down system")
+    #             self.back_led.on()
+    #             os.system("sudo poweroff")
+    #         else:
+    #             logger.debug("Shutting down Pi process")
+    #             # This throws some gpiozero related error on exit, but oh well
+    #             os.kill(self.pid, signal.SIGUSR1)
 
-    def default_splash_screen(self):
-        smiley = (
-            0b00000,
-            0b01010,
-            0b01010,
-            0b00000,
-            0b10001,
-            0b10001,
-            0b01110,
-            0b00000,
-        )
-        self.lcd.create_char(0, smiley)
-        s = chr(0)
-        lines = [f"{s*3}RASPBERRY{s*4}", f"{s*7}PI{s*7}"]
-        # framebuffers = create_framebuffers(lines)
-        # self.write_framebuffers(framebuffers)
-        self.stream_lines(lines)
+    # def default_splash_screen(self):
+    #     smiley = (
+    #         0b00000,
+    #         0b01010,
+    #         0b01010,
+    #         0b00000,
+    #         0b10001,
+    #         0b10001,
+    #         0b01110,
+    #         0b00000,
+    #     )
+    #     self.lcd.create_char(0, smiley)
+    #     s = chr(0)
+    #     lines = [f"{s*3}RASPBERRY{s*4}", f"{s*7}PI{s*7}"]
+    #     # framebuffers = create_framebuffers(lines)
+    #     # self.write_framebuffers(framebuffers)
+    #     self.stream_lines(lines)
 
-    def default_startup_text(self):
-        # smiley = (
-        #     0b00000,
-        #     0b01010,
-        #     0b01010,
-        #     0b00000,
-        #     0b10001,
-        #     0b10001,
-        #     0b01110,
-        #     0b00000,
-        # )
-        # self.lcd.create_char(0, smiley)
-        # s = chr(0)
-        startup_steps_lines = [
-            ['Loading', '.'],
-            ['Loading', '..'],
-            ['Loading', '...']
-            # [f"{s*3}RASPBERRY{s*4}", f"{s*7}PI{s*7}"]
-        ]
-        self.stream_multiples_of_lines(startup_steps_lines)
-        # for lines in startup_steps_lines:
-        #     framebuffers = create_framebuffers(lines)
-        #     logger.debug(framebuffers)
-        #     self.write_framebuffers(framebuffers)
-        #     time.sleep(0.5)
+    # def default_startup_text(self):
+    #     # smiley = (
+    #     #     0b00000,
+    #     #     0b01010,
+    #     #     0b01010,
+    #     #     0b00000,
+    #     #     0b10001,
+    #     #     0b10001,
+    #     #     0b01110,
+    #     #     0b00000,
+    #     # )
+    #     # self.lcd.create_char(0, smiley)
+    #     # s = chr(0)
+    #     startup_steps_lines = [
+    #         ['Loading', '.'],
+    #         ['Loading', '..'],
+    #         ['Loading', '...']
+    #         # [f"{s*3}RASPBERRY{s*4}", f"{s*7}PI{s*7}"]
+    #     ]
+    #     self.stream_multiples_of_lines(startup_steps_lines)
+    #     # for lines in startup_steps_lines:
+    #     #     framebuffers = create_framebuffers(lines)
+    #     #     logger.debug(framebuffers)
+    #     #     self.write_framebuffers(framebuffers)
+    #     #     time.sleep(0.5)
 
-    def set_startup_method(self):
-        self.default_startup_text()
-        self.default_splash_screen()
+    # def set_startup_method(self):
+    #     self.default_startup_text()
+    #     self.default_splash_screen()
+
+    def add_to_print_jobs_queue(self, framebuffer, delay=0):
+        print_job = (framebuffer, delay)
+        self.print_jobs.put(print_job)
 
     def front_button__when_pressed(self):
         logger.debug(FUNCTION_CALL_MSG)
@@ -278,9 +260,77 @@ class RaspberryPi(Process):
         self.back_led.off()
 
     def run(self):
-        self.lcd_cleanup()
-        self.startup_method()
+        # self.lcd_cleanup()
+        # self.startup_method()
+        self.lcd_printer.start()
         signal.pause()
+
+    class LCDFramebufferPrinter(Process):
+        def __init__(self, pi):
+            super().__init__()
+            logger.debug(f"{FUNCTION_CALL_MSG}, {__class__}")
+            self.pi = pi
+
+        def validate_print_job(self, print_job):
+            logger.debug(FUNCTION_CALL_MSG)
+            if len(print_job) != 2 or not isinstance(print_job, tuple):
+                raise ValueError(f"print_job must be tuple with 2 elements ({print_job})")
+            if not isinstance(print_job[0], list):
+                raise ValueError(f"1st element must be a list ({print_job})")
+            if not isinstance(print_job[1], int):
+                raise ValueError(f"2nd element must be an int ({print_job})")
+            if len(print_job[0]) != self.pi.lcd_rows:
+                raise ValueError(f"1st element must be list with {self.pi.lcd_rows} elements ({print_job})")
+            if any([len(s) != self.pi.lcd_cols for s in print_job[0]]):
+                raise ValueError(f"1st element must be list with only {self.pi.lcd_cols} long elements ({print_job})")
+            print("Valid Tuple")
+
+        def lcd_cleanup(self):
+            logger.debug(FUNCTION_CALL_MSG)
+            self.pi.lcd.home()
+            self.pi.lcd.clear()
+
+        def write_framebuffer(self, framebuffer):
+            """Writes single framebuffer to LCD screen.
+            Framebuffer must fill entire LCD screen.
+            Valid framebuffer for 2x16 LCD:
+            ["I Am A Raspberry", "Pi                "]
+            No need to clear LCD since all cells will be overwritten
+            """
+            logger.debug(FUNCTION_CALL_MSG)
+            self.pi.lcd.home()
+            for row in framebuffer:
+                try:
+                    self.pi.lcd.write_string(row)
+                    self.pi.lcd.crlf()
+                except Exception as e:
+                    print("Something went wrong:")
+                    print(e)
+                    return
+
+        def run_print_job(self, print_job):
+            logger.debug(FUNCTION_CALL_MSG)
+            try:
+                self.validate_print_job(print_job)
+            except ValueError as e:
+                logger.debug(e)
+            framebuffer, delay = print_job
+            # If all framebuffers are blank
+            if framebuffer == [" " * self.pi.lcd_cols] * self.pi.lcd_rows:
+                self.lcd_cleanup()
+            else:
+                self.write_framebuffer(framebuffer)
+            # Always sleeping for at least 0.2
+            # to make sure data is done transmitting
+            # to the LCD before next write.
+            delay = max(delay, 0.2)
+            time.sleep(delay)
+
+        def run(self):
+            logger.debug(FUNCTION_CALL_MSG)
+            while True:
+                print_job = self.pi.print_jobs.get()
+                self.run_print_job(print_job)
 
 
 class ButtonWrapper(Button):
